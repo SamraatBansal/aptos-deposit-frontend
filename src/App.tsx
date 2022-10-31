87bea3d3fa49d6fc5f8974d127ecf1e280db83d6
SamraatBansal <samraatbansal7@gmail.com>
2022-10-31 12:43:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import {
+  WalletProvider,
+  HyperPayWalletAdapter,
+  AptosWalletAdapter,
+  HippoExtensionWalletAdapter,
+  MartianWalletAdapter,
+  FewchaWalletAdapter,
+  PontemWalletAdapter,
+  RiseWalletAdapter
+} from '@manahippo/aptos-wallet-adapter';
+import { Col, message, Row, Switch, Typography } from 'antd';
+import MainPage from './pages';
+
+const App: React.FC = () => {
+  const queryParams = new URLSearchParams(window.location.search);
+  const autoConnect = queryParams.get('autoConnect')?.toLowerCase() === 'true' ? true : false;
+  const [auto, setAuto] = useState(autoConnect);
+
+  useEffect(() => {
+    const url = new URL(window.location as any);
+    url.searchParams.set('autoConnect', auto ? 'true' : 'false');
+    window.history.pushState({}, '', url);
+  }, [auto]);
+
+  const wallets = useMemo(
+    () => [
+      new HyperPayWalletAdapter(),
+      new HippoExtensionWalletAdapter(),
+      new MartianWalletAdapter(),
+      new AptosWalletAdapter(),
+      new FewchaWalletAdapter(),
+      new PontemWalletAdapter(),
+      new RiseWalletAdapter()
+    ],
+    []
+  );
+
+  const onChange = (checked: boolean) => {
+    setAuto(checked);
+  };
+
+  return (
+    <WalletProvider
+      wallets={wallets}
+      autoConnect={auto}
+      onError={(error: Error) => {
+        console.log('wallet errors: ', error);
+        message.error(error.message);
+      }}>
+      <Row gutter={8}>
+        <Col>
+          <Typography>Enable Auto Connect:</Typography>
+        </Col>
+        <Col>
+          <Switch checked={auto} onChange={onChange} />
+        </Col>
+      </Row>
+      <Row gutter={8}>
+        <Col>
+          <Typography>Auto Connect:</Typography>
+        </Col>
+        <Col>
+          <Typography>{auto ? 'True' : 'False'}</Typography>
+        </Col>
+      </Row>
+      <MainPage />
+    </WalletProvider>
+  );
+};
+
+export default App;
